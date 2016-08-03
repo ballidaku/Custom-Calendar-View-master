82250bd6e2f9f5be8f10d1582170640df786ee33
Brst-Pc17 <brst-pc17@localhost.localdomain>
2016-08-03 06:48:53 -0400

    Initial commit

---
+++ library/src/com/imanoweb/calendarview/CalendarListener.java
@@ -0,0 +1,14 @@
+package com.imanoweb.calendarview;
+
+import java.util.Date;
+
+/**
+ * Created by Nilanchala on 9/14/15.
+ */
+public interface CalendarListener {
+    void onDateSelected(Date date);
+
+    void onMonthChanged(Date time);
+
+    void onYearChanged(Date time);
+}
